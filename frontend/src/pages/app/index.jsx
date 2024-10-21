88ef1e7e6a2a6a355952d3fe3728567e3b4bee94
vegezika46 <ra08562337579@acaonsfatima.org.br>
2024-10-21 11:53:55 -0300

    depois

--- frontend/src/pages/app/index.jsx
+++ frontend/src/pages/app/index.jsx
@@ -3,39 +3,19 @@ import { Link } from 'react-router-dom';
 
 export default function App() {
 
-  return (
-    <div className="App pagina">
+    return(
+        <div className='pagina-cliente pagina'>
+            <header className='cabecalho'>
+                <div className='area-cima'>
+                    <img className='logo' src="/assets/images/profileicon.png"/>
+                    <p>Sobre</p>
+                    <p>Serviços</p>
+                    <p>Loja</p>
+                </div>
+                <div className='area-links'>
 
-      <header className='cabecalho'>
-        <img src="/assets/images/logo.png" className='logo'/>
-        <div className='area-links'>
-          <p>SOBRE NÓS</p>
-          <p>SERVIÇOS</p>
+                </div>
+            </header>
         </div>
-          <img src="/assets/images/text.png" className='text-foto' />
-        <div className='area-links2'>
-          <p>SHOP</p>
-          <p>CONTATO</p>
-        </div>
-        <img src="/assets/images/contat.png" className='contat-foto' />
-      </header>
-      <div className='texto'>
-        <h1 className='titulo-text'>Curta nossos serviços de barbeiro</h1>
-        <p className='introducao'>Estabeleça seu novo visual incrível com os serviços da Ale Barber! De cortes de cabelo até a barbas perfeitas, você pode experimentar o melhor nivel de barbaria na área</p>
-      </div>
-
-
-        <div className='botoes'>
-          <Link to="/client" className='botao'>Cadastrar</Link>
-          <Link to="/client" className='botao'>Entrar</Link>
-        </div>
-        <div className='linha'></div>
-        <div className='redes'>
-          <img src="/assets/images/whatsapp.png" className='imgContat'/>
-          <img src="/assets/images/facebook.png" className='imgContat'/>
-          <img src="/assets/images/instagram.png" className='imgContat'/>
-        </div>
-    </div>
-  );
+    );
 }
-
